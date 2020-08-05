83fafd44257a1c39fd060a7b7223e767f6200456
emptyaf <63400134+emptyaf@users.noreply.github.com>
2020-08-05 08:40:44 +0700

    Update phanbiet.py

--- works/Task1_Phanbietdat_Canhodichvu/phanbiet.py
+++ works/Task1_Phanbietdat_Canhodichvu/phanbiet.py
@@ -1,7 +1,10 @@
 
+import re
+import json
+from utils import remove_accents
 
 def phanbiet(dict_input: dict) -> int:
-    """Phân biệt cái gì đó ahihi, mọi người sửa lại dòng này cho đúng nhé.
+    """Phân biệt căn hộ dịch vụ .
 
     Arguments:
         inp_str {dict} -- dict chứa 3 thông tin sau:
@@ -15,7 +18,20 @@ def phanbiet(dict_input: dict) -> int:
     realestate_type = dict_input['realestate_type']
 
     ## code goes here
+    content = dict_input['content']
+    if (realestate_type == 3):
+        # remove Vietnameses accent from content using built-in function
+        new_content = remove_accents(content)
 
+        # find all the post that has such keyword by using regex rules
+        regex_rule = "can\s+ho\s+dich\s+vu|chdv"
+        
+        result = re.search(regex_rule, new_content)
+
+        if (result is not None):
+            realestate_type = 2
+        else:
+            realestate_type = 3
 
     ############################
     ## Return
